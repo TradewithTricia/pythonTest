07e656f34606ce96eea650f74fe43cd02a636e07
b-vennes <b.n.vennes@gmail.com>
2016-11-21 10:09:03 -0800

    Added starting code for isbn number recognizing

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, session
-import twilio.twiml, random
+import twilio.twiml, random, re
 
 # The session object makes use of a secret key.
 SECRET_KEY = 'a secret key'
@@ -9,8 +9,17 @@ app.config.from_object(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def replyMethod():
     
+    #isbn = re.compile(^(97(8|9))?\d{9}(\d|X)$)
+    
     number = request.form['From']
     message_body = request.form['Body']
+    
+    #isbnNumbers = isbn.findall(message_body)
+    
+    #message = ''
+    
+    #if isbnNumbers.amount() == 0:
+    #    message = 'found isbn number'
 
     greetings = ['Hello! ', 'Hi! ', 'Hey! ']
 
